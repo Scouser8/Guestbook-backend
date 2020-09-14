74f6ed27039a5a5636ada575ed1a85b16c2bbdea
Muhammad Abd Al-Baqi <45911940+Scouser8@users.noreply.github.com>
2020-09-14 21:16:33 +0200

    Step3 - Added registration checking if the username exists, & used bcrypt for hashing passwords / Added user authentication when logging in.

--- Routes/UsersRoute.js
+++ Routes/UsersRoute.js
@@ -1,21 +1,62 @@
 const express = require("express");
 const router = express.Router();
 const Users = require("../Models/User");
+const bcrypt = require("bcrypt");
 
-router.post("/login", (req, res) => {
-  const obj = JSON.parse(JSON.stringify(req.body));
-  console.log(req.body);
-  res.send(req.body);
-});
-
+//Create a new user
 router.post("/register", (req, res) => {
   const newUser = req.body;
-  console.log(newUser);
-  Users.create(newUser, (err, data) => {
-    if (err) {
-      res.status(500).send(err);
+  console.log(newUser.password);
+
+  Users.findOne({ user_name: newUser.user_name }, (err, user) => {
+    if (err) throw err;
+    else if (user) {
+      res.send("Invalid");
     } else {
-      res.status(201).send("New user created successfully.");
+      bcrypt.genSalt(10, (err, salt) =>
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          //   if(Users.find({password: hash}))
+          console.log(Users.find({ password: hash }));
+          newUser.password = hash;
+
+          console.log(`Hashed password: ${newUser.password}`);
+
+          Users.create(newUser, (err, data) => {
+            if (err) {
+              res.status(500).send(err);
+            } else {
+              res.status(201).send("New user created successfully.");
+            }
+          });
+        })
+      );
+    }
+  });
+});
+
+//Authenticate user
+router.post("/login", (req, res) => {
+  const userData = req.body;
+
+  Users.findOne({ user_name: userData.user_name }, (err, user) => {
+    if (err) throw err;
+    else if (!user) {
+      res.send("username not found");
+    } else {
+      bcrypt.compare(userData.password, user.password, (err, isMatched) => {
+        if (err) {
+          console.log("Error");
+          throw err;
+        }
+        if (isMatched) {
+          console.log("Logged In");
+          res.status(201).send("You have logged in successfully!");
+        } else {
+          console.log("Failed");
+          res.send("Wrong");
+        }
+      });
     }
   });
 });
